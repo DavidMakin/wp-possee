4fbd994277a10b6a4dd3926bee67c9b3ad5f1873
David Makin <sleep-er@sleep-er.co.uk>
2026-05-17 15:01:49 +0100

    fix(mu): inline SVG heart instead of unicode char

--- mu-plugins/theme-styles.php
+++ mu-plugins/theme-styles.php
@@ -305,7 +305,7 @@ add_action( 'wp_footer', function () {
 		if (count > 0) {
 			var label = document.createElement('span');
 			label.className = 'likes-label';
-			label.innerHTML = '♥ ' + count;
+			label.innerHTML = '<svg viewBox="0 0 24 24" width="14" height="14" style="vertical-align:middle;fill:#e25555;margin-right:2px"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg> ' + count;
 			likesEl.insertBefore(label, likesEl.firstChild);
 		}
 	}
